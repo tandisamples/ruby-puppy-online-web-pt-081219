b35b1da4a867cab9373cb0c2aea30efc77a858db
Tandi Samples <tandisamples@gmail.com>
2019-09-15 18:37:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+class Dog 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    save
+  end 
+  attr_reader :name
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each{|el| puts el.name}
+  end
+
+  def save 
+    @@all << self
+  end 
+
+  def self.clear_all
+    @@all = []
+  end
+end 
